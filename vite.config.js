ab044f076071a904811b2644b930066fc37e82f8
Paco Fuentes <pacofuentes.work@gmail.com>
2023-11-10 09:36:17 +0100

    styles: cleaning vite templates

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000', //vuestro express
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      cors:"false"
+    },
+  },
+    
 })
