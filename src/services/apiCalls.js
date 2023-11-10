9ae62250a3c9496ffb7eb4adb6cf81bb477ec437
Paco Fuentes <pacofuentes.work@gmail.com>
2023-11-10 12:11:50 +0100

    fix: apiCalls and viteconfig

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -6,7 +6,7 @@ export const logUser = async (body) => {
     console.log(body);
 
     //SIMULACRO DE CONEXION REAL A API
-    return await axios.post(`elendpointdemipreciosobackend`, body);
+    // return await axios.post(`elendpointdemipreciosobackend`, body);
 
     return `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMsImVtYWlsIjoiZGF2aWRAZGF2aWQuY29tIiwiaWF0IjoxNjk5NTMwMDY1LCJleHAiOjE2OTk1NDA4NjV9.UNMBvL7RwP6JylDq9Ut4r5ach9HGVKqBm-5yv0-w_B4`;
 
