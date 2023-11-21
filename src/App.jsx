eef1213e2228cfa5d0aa88b21b5aef7579b04224
Paco Fuentes <pacofuentes.work@gmail.com>
2023-11-21 17:57:42 +0100

    feat: no draggable images

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
 
-
 // views and components
 import { Header } from './common/Header/Header'
 import { Body } from './pages/Body/Body'
 
-
 function App() {
-  
-
   return (
     <>
       <Header />
